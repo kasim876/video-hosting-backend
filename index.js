965aa1bb6d5623077232cef0b49176a370af9d12
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-02-18 22:41:42 +0200

    add static server

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+
 const sequelize = require('./db');
-const models = require('./models/models');
+const models = require('./models');
+const router = require('./router');
 
 const port = process.env.PORT || 9000;
 
@@ -10,6 +14,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static/thumbnail')));
+app.use(express.static(path.resolve(__dirname, 'static/video')));
+app.use(fileUpload({}));
+app.use('/api', router);
 
 const start = async () => {
   try {
