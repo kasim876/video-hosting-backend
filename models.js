5e8eb3a0ef92761838ae652556f19b1f98afd75f
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-02-25 15:19:00 +0200

    add like entity

--- models.js
+++ models.js
@@ -3,47 +3,47 @@ const VideoEntity = require('./video/video.model');
 const SubscriptionEntity = require('./subscription/subscription.model');
 const CommentEntity = require('./comment/comment.model');
 
-UserEntity.hasMany(VideoEntity, {
-  foreignKey: {
-    name: 'user_id',
-    allowNull: false,
-  },
-});
+// UserEntity.hasMany(VideoEntity, {
+//   foreignKey: {
+//     name: 'user_id',
+//     allowNull: false,
+//   },
+// });
 
-VideoEntity.belongsTo(UserEntity, {
-  foreignKey: {
-    name: 'user_id',
-    allowNull: false,
-  },
-});
+// VideoEntity.belongsTo(UserEntity, {
+//   foreignKey: {
+//     name: 'user_id',
+//     allowNull: false,
+//   },
+// });
 
-UserEntity.hasMany(CommentEntity, {
-  foreignKey: {
-    name: 'user_id',
-    allowNull: false,
-  },
-});
+// UserEntity.hasMany(CommentEntity, {
+//   foreignKey: {
+//     name: 'user_id',
+//     allowNull: false,
+//   },
+// });
 
-CommentEntity.belongsTo(UserEntity, {
-  foreignKey: {
-    name: 'user_id',
-    allowNull: false,
-  },
-});
+// CommentEntity.belongsTo(UserEntity, {
+//   foreignKey: {
+//     name: 'user_id',
+//     allowNull: false,
+//   },
+// });
 
-VideoEntity.hasMany(CommentEntity, {
-  foreignKey: {
-    name: 'video_id',
-    allowNull: false,
-  },
-});
+// VideoEntity.hasMany(CommentEntity, {
+//   foreignKey: {
+//     name: 'video_id',
+//     allowNull: false,
+//   },
+// });
 
-CommentEntity.belongsTo(VideoEntity, {
-  foreignKey: {
-    name: 'video_id',
-    allowNull: false,
-  },
-});
+// CommentEntity.belongsTo(VideoEntity, {
+//   foreignKey: {
+//     name: 'video_id',
+//     allowNull: false,
+//   },
+// });
 
 UserEntity.hasMany(SubscriptionEntity, {
   foreignKey: {
@@ -74,8 +74,8 @@ SubscriptionEntity.belongsTo(UserEntity, {
 });
 
 module.exports = {
-  User,
-  Video,
-  Comment,
-  Subscriptions,
+  UserEntity,
+  VideoEntity,
+  CommentEntity,
+  SubscriptionEntity,
 };
