5913c285e88bc942f223349a3eab0a133b6f6aa6
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-06 08:21:58 +0300

    init typescript

--- entities/user/user.router.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express');
-const router = new Router();
-
-const userController = require('./user.controller');
-
-router.post('/register', userController.registration);
-router.post('/login', userController.login);
-
-module.exports = router;
--- utils/generateJwtToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateJwt = (id, email, name, avatarPath) => {
-  return jwt.sign({id, email, name, avatarPath}, process.env.JWT_SECRET, {
-    expiresIn: '24h',
-  });
-};
-
-module.exports = generateJwt;
