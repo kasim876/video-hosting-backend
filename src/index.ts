98fc2d340bd5fb64b53b8bc9b1615d5ecca4ec0b
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-10 12:40:46 +0300

    add static server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import * as path from 'path';
 import dotenv from 'dotenv';
 import {createExpressServer} from 'routing-controllers';
+import serveStatic from 'serve-static';
 
 dotenv.config();
 
@@ -13,5 +15,9 @@ const app = createExpressServer({
   controllers: [UserController, SubscriptionsController],
   routePrefix: '/api',
 });
+
+app.use(serveStatic(path.join(__dirname, 'static/video')));
+app.use(serveStatic(path.join(__dirname, 'static/thumbnail')));
+app.use(serveStatic(path.join(__dirname, 'static/avatar')));
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
