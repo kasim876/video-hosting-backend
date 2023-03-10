b5fd1da4a3fdfd41cc28e8393b8be0fcaa9f0d2e
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-10 21:11:23 +0300

    add video controller

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
+import * as path from 'path';
 import dotenv from 'dotenv';
 import {createExpressServer} from 'routing-controllers';
+import serveStatic from 'serve-static';
 
 dotenv.config();
 
 import {UserController} from './controller/user-controller';
 import {SubscriptionsController} from './controller/subscriptions-controller';
+import {VideoController} from './controller/video-controller';
 
 const PORT = process.env.PORT || 9000;
 
 const app = createExpressServer({
   cors: true,
-  controllers: [UserController, SubscriptionsController],
+  controllers: [UserController, SubscriptionsController, VideoController],
   routePrefix: '/api',
 });
+
+app.use(serveStatic(path.join(__dirname, 'static/video')));
+app.use(serveStatic(path.join(__dirname, 'static/thumbnail')));
+app.use(serveStatic(path.join(__dirname, 'static/avatar')));
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
