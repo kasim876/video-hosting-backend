1b12472dd4cdcac77cf8d13c7f9957b53fb44b09
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-12 13:06:32 +0300

    add type-orm connection

--- src/db.ts
+++ src/db.ts
@@ -1,21 +1,30 @@
-import {Client} from 'pg';
+import 'reflect-metadata';
+import {DataSource} from 'typeorm';
+
+import {CommentEntity} from './entity/comment-entity';
+import {SubscriptionEntity} from './entity/subscription-entity';
+import {UserEntity} from './entity/user-entity';
+import {VideoEntity} from './entity/video-entity';
 
 const username = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const host = process.env.DB_HOST;
 const database = process.env.DB_NAME;
-const port = process.env.DB_PORT;
+const port = +process.env.DB_PORT;
 
-const connectionString = `postgres://${username}:${password}@${host}:${port}/${database}`;
-
-const db = new Client(connectionString);
-
-db.connect(err => {
-  if (err) {
-    console.error('connection to database error', err.stack);
-  } else {
-    console.log('connected to database success');
-  }
+export const db = new DataSource({
+  type: 'postgres',
+  host: host,
+  port: port,
+  database: database,
+  username: username,
+  password: password,
+  synchronize: true,
+  entities: [CommentEntity, SubscriptionEntity, UserEntity, VideoEntity],
 });
 
-export default db;
+db.initialize()
+  .then(() => {
+    console.log('database connecting success');
+  })
+  .catch(error => console.log(error));
