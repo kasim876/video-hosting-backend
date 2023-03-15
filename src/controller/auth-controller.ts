b9b2de2908c002ce863d77660692c2835743cf08
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-15 19:55:45 +0300

    update auth controller

--- src/controller/auth-controller.ts
+++ src/controller/auth-controller.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-import {Response} from 'express';
 import {Body, JsonController, Post, Res} from 'routing-controllers';
 
 import {AuthService} from '../service/auth-service';
@@ -7,15 +6,15 @@ import {AuthDto} from '../types/auth-dto';
 
 @JsonController('/auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService = new AuthService()) {}
+  private readonly authService: AuthService = new AuthService();
 
   @Post('/login')
-  async login(@Body() dto: AuthDto, @Res() res: Response) {
+  async login(@Body() dto: AuthDto) {
     return this.authService.login(dto);
   }
 
   @Post('/register')
-  async register(@Body() dto: AuthDto, @Res() res: Response) {
+  async register(@Body() dto: AuthDto) {
     return this.authService.register(dto);
   }
 }
