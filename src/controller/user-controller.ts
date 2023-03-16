903d8bed2ec47462cb8229528c67f57ac9219e07
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-03-16 11:26:34 +0300

    update user controller

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,41 +1,35 @@
 import 'reflect-metadata';
-import {
-  Body,
-  CurrentUser,
-  Get,
-  JsonController,
-  Param,
-  Patch,
-  Put,
-} from 'routing-controllers';
+import {Body, CurrentUser, Get, JsonController, Param, Patch, Put} from 'routing-controllers';
 
 import {UserDto} from '../types/user-dto';
 import {UserService} from '../service/user-service';
 
 @JsonController('/user')
 export class UserController {
-  constructor(private readonly userService: UserService = new UserService()) {}
+  private readonly userService: UserService = new UserService();
 
   @Get()
-  async getAll() {
+  getAll() {
     return this.userService.getAll();
   }
 
   @Get('/profile')
-  async getProfile(@CurrentUser({required: true}) {id}) {
+  getProfile(@CurrentUser({required: true}) id: number) {
     return this.userService.getOne(id);
   }
 
+  @Get('/:id')
+  getUser(@Param('id') id: string) {
+    return this.userService.getOne(+id);
+  }
+
   @Put()
-  async updateUser(@CurrentUser({required: true}) {id}, @Body() dto: UserDto) {
+  updateUser(@CurrentUser({required: true}) id: number, @Body() dto: UserDto) {
     return this.userService.updateProfile(id, dto);
   }
 
   @Patch('/subscribe/:channelId')
-  async subscribeToChannel(
-    @CurrentUser({required: true}) {id},
-    @Param('channelId') channelId: number,
-  ) {
-    return this.userService.subscribe(id, channelId);
+  subscribeToChannel(@CurrentUser({required: true}) id: number, @Param('channelId') channelId: string) {
+    return this.userService.subscribe(id, +channelId);
   }
 }
